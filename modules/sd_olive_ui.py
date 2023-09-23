953706bacf986eb573c7c44b40d8ff67a23e794c
Seunghoon Lee <lshqqytiger@naver.com>
2023-09-23 23:17:19 +0900

    Update Olive UI.

--- modules/sd_olive_ui.py
+++ modules/sd_olive_ui.py
@@ -2,9 +2,15 @@ import os
 import json
 import torch
 import shutil
-from typing import Union
+from typing import Union, Tuple, List
 from pathlib import Path
-from diffusers import OnnxRuntimeModel, OnnxStableDiffusionPipeline, StableDiffusionPipeline, StableDiffusionXLPipeline
+from diffusers import (
+    DiffusionPipeline,
+    OnnxRuntimeModel,
+    OnnxStableDiffusionPipeline,
+    StableDiffusionPipeline,
+    StableDiffusionXLPipeline,
+)
 from optimum.onnxruntime import ORTStableDiffusionXLPipeline
 from olive.model import ONNXModel
 from olive.workflows import run as olive_run
@@ -14,55 +20,121 @@ from modules.paths_internal import sd_configs_path, models_path
 from modules.sd_models import unload_model_weights
 from modules.sd_onnx_utils import load_pipeline
 
-available_sampling_methods = ["pndm", "lms", "heun", "euler", "euler-ancestral", "dpm", "ddim"]
+available_sampling_methods = [
+    "pndm",
+    "lms",
+    "heun",
+    "euler",
+    "euler-ancestral",
+    "dpm",
+    "ddim",
+]
 
-def optimize_from_ckpt(checkpoint: str, vae_id: str, vae_subfolder: str, unoptimized_dir: str, optimized_dir: str, safety_checker: bool, text_encoder: bool, text_encoder_2: bool, unet: bool, vae_decoder: bool, vae_encoder: bool, scheduler_type: str, use_fp16: bool, sample_size: Union[tuple[int, int], int], olive_merge_lora: bool, *olive_merge_lora_inputs):
+
+def ready(unoptimized_dir: str, optimized_dir: str):
     unload_model_weights()
 
     unoptimized_dir = Path(models_path) / "ONNX" / unoptimized_dir
     optimized_dir = Path(models_path) / "ONNX-Olive" / optimized_dir
 
     shutil.rmtree("footprints", ignore_errors=True)
-    shutil.rmtree(unoptimized_dir, ignore_errors=True)
     shutil.rmtree(optimized_dir, ignore_errors=True)
 
-    pipeline = StableDiffusionPipeline.from_single_file(os.path.join(models_path, "Stable-diffusion", checkpoint), torch_dtype=torch.float32, requires_safety_checker=False, scheduler_type=scheduler_type)
+    return unoptimized_dir, optimized_dir
+
+
+def optimize_sd_from_ckpt(
+    checkpoint: str,
+    vae_id: str,
+    vae_subfolder: str,
+    unoptimized_dir: str,
+    optimized_dir: str,
+    safety_checker: bool,
+    text_encoder: bool,
+    unet: bool,
+    vae_decoder: bool,
+    vae_encoder: bool,
+    scheduler_type: str,
+    use_fp16: bool,
+    sample_size: Union[Tuple[int, int], int],
+    olive_merge_lora: bool,
+    *olive_merge_lora_inputs,
+):
+    unoptimized_dir, optimized_dir = ready(unoptimized_dir, optimized_dir)
+    shutil.rmtree(unoptimized_dir, ignore_errors=True)
+
+    pipeline = StableDiffusionPipeline.from_single_file(
+        os.path.join(models_path, "Stable-diffusion", checkpoint),
+        torch_dtype=torch.float32,
+        requires_safety_checker=False,
+        scheduler_type=scheduler_type,
+    )
     pipeline.save_pretrained(unoptimized_dir)
 
     hw_synced = isinstance(sample_size, int)
 
-    optimize(unoptimized_dir, optimized_dir, pipeline, vae_id, vae_subfolder, safety_checker, text_encoder, text_encoder_2, unet, vae_decoder, vae_encoder, use_fp16, sample_size if hw_synced else sample_size[0], sample_size if hw_synced else sample_size[1], olive_merge_lora, *olive_merge_lora_inputs)
-
-    return ["Optimization complete."]
-
-def optimize_from_onnx(model_id: str, vae_id: str, vae_subfolder: str, unoptimized_dir: str, optimized_dir: str, safety_checker: bool, text_encoder: bool, text_encoder_2: bool, unet: bool, vae_decoder: bool, vae_encoder: bool, use_fp16: bool, sample_size: Union[tuple[int, int], int], olive_merge_lora: bool, *olive_merge_lora_inputs):
-    unload_model_weights()
-
-    unoptimized_dir = Path(models_path) / "ONNX" / unoptimized_dir
-    optimized_dir = Path(models_path) / "ONNX-Olive" / optimized_dir
-
-    shutil.rmtree("footprints", ignore_errors=True)
-    shutil.rmtree(optimized_dir, ignore_errors=True)
-
-    if os.path.isdir(unoptimized_dir):
-        pipeline = load_pipeline(unoptimized_dir, text_encoder_2, torch_dtype=torch.float32, requires_safety_checker=False, local_files_only=True)
-    else:
-        pipeline = (StableDiffusionXLPipeline if text_encoder_2 else StableDiffusionPipeline).from_pretrained(model_id, torch_dtype=torch.float32, requires_safety_checker=False)
-        pipeline.save_pretrained(unoptimized_dir)
-
-    hw_synced = isinstance(sample_size, int)
-
-    optimize(unoptimized_dir, optimized_dir, pipeline, vae_id, vae_subfolder, safety_checker, text_encoder, text_encoder_2, unet, vae_decoder, vae_encoder, use_fp16, sample_size if hw_synced else sample_size[0], sample_size if hw_synced else sample_size[1], olive_merge_lora, *olive_merge_lora_inputs)
-
-    return ["Optimization complete."]
-
-def optimize(unoptimized_dir: Path, optimized_dir: Path, pipeline, vae_id: str, vae_subfolder: str, safety_checker: bool, text_encoder: bool, text_encoder_2: bool, unet: bool, vae_decoder: bool, vae_encoder: bool, use_fp16: bool, sample_height: int, sample_width: int, olive_merge_lora: bool, *olive_merge_lora_inputs):
-    model_info = {}
-    submodels = []
-    is_sdxl = text_encoder_2
+    submodels = list()
 
     if safety_checker:
         submodels.append("safety_checker")
+    if text_encoder:
+        submodels.append("text_encoder")
+    if unet:
+        submodels.append("unet")
+    if vae_decoder:
+        submodels.append("vae_decoder")
+    if vae_encoder:
+        submodels.append("vae_encoder")
+
+    optimize(
+        unoptimized_dir,
+        optimized_dir,
+        pipeline,
+        vae_id,
+        vae_subfolder,
+        use_fp16,
+        sample_size if hw_synced else sample_size[0],
+        sample_size if hw_synced else sample_size[1],
+        submodels,
+        olive_merge_lora,
+        *olive_merge_lora_inputs,
+    )
+
+    return ["Optimization complete."]
+
+
+def optimize_sdxl_from_ckpt(
+    checkpoint: str,
+    vae_id: str,
+    vae_subfolder: str,
+    unoptimized_dir: str,
+    optimized_dir: str,
+    text_encoder: bool,
+    text_encoder_2: bool,
+    unet: bool,
+    vae_decoder: bool,
+    vae_encoder: bool,
+    scheduler_type: str,
+    use_fp16: bool,
+    sample_size: Union[Tuple[int, int], int],
+    olive_merge_lora: bool,
+    *olive_merge_lora_inputs,
+):
+    unoptimized_dir, optimized_dir = ready(unoptimized_dir, optimized_dir)
+    shutil.rmtree(unoptimized_dir, ignore_errors=True)
+
+    pipeline = StableDiffusionXLPipeline.from_single_file(
+        os.path.join(models_path, "Stable-diffusion", checkpoint),
+        torch_dtype=torch.float32,
+        requires_safety_checker=False,
+        scheduler_type=scheduler_type,
+    )
+    pipeline.save_pretrained(unoptimized_dir)
+
+    hw_synced = isinstance(sample_size, int)
+
+    submodels = list()
+
     if text_encoder:
         submodels.append("text_encoder")
     if text_encoder_2:
@@ -73,6 +145,167 @@ def optimize(unoptimized_dir: Path, optimized_dir: Path, pipeline, vae_id: str, 
         submodels.append("vae_decoder")
     if vae_encoder:
         submodels.append("vae_encoder")
+
+    optimize(
+        unoptimized_dir,
+        optimized_dir,
+        pipeline,
+        vae_id,
+        vae_subfolder,
+        use_fp16,
+        sample_size if hw_synced else sample_size[0],
+        sample_size if hw_synced else sample_size[1],
+        submodels,
+        olive_merge_lora,
+        *olive_merge_lora_inputs,
+    )
+
+    return ["Optimization complete."]
+
+
+def optimize_sd_from_onnx(
+    model_id: str,
+    vae_id: str,
+    vae_subfolder: str,
+    unoptimized_dir: str,
+    optimized_dir: str,
+    safety_checker: bool,
+    text_encoder: bool,
+    unet: bool,
+    vae_decoder: bool,
+    vae_encoder: bool,
+    use_fp16: bool,
+    sample_size: Union[Tuple[int, int], int],
+    olive_merge_lora: bool,
+    *olive_merge_lora_inputs,
+):
+    unoptimized_dir, optimized_dir = ready(unoptimized_dir, optimized_dir)
+
+    if os.path.isdir(unoptimized_dir):
+        pipeline = load_pipeline(
+            unoptimized_dir,
+            False,
+            torch_dtype=torch.float32,
+            requires_safety_checker=False,
+            local_files_only=True,
+        )
+    else:
+        pipeline = StableDiffusionPipeline.from_pretrained(
+            model_id, torch_dtype=torch.float32, requires_safety_checker=False
+        )
+        pipeline.save_pretrained(unoptimized_dir)
+
+    hw_synced = isinstance(sample_size, int)
+
+    submodels = list()
+
+    if safety_checker:
+        submodels.append("safety_checker")
+    if text_encoder:
+        submodels.append("text_encoder")
+    if unet:
+        submodels.append("unet")
+    if vae_decoder:
+        submodels.append("vae_decoder")
+    if vae_encoder:
+        submodels.append("vae_encoder")
+
+    optimize(
+        unoptimized_dir,
+        optimized_dir,
+        pipeline,
+        vae_id,
+        vae_subfolder,
+        use_fp16,
+        sample_size if hw_synced else sample_size[0],
+        sample_size if hw_synced else sample_size[1],
+        submodels,
+        olive_merge_lora,
+        *olive_merge_lora_inputs,
+    )
+
+    return ["Optimization complete."]
+
+
+def optimize_sdxl_from_onnx(
+    model_id: str,
+    vae_id: str,
+    vae_subfolder: str,
+    unoptimized_dir: str,
+    optimized_dir: str,
+    text_encoder: bool,
+    text_encoder_2: bool,
+    unet: bool,
+    vae_decoder: bool,
+    vae_encoder: bool,
+    use_fp16: bool,
+    sample_size: Union[Tuple[int, int], int],
+    olive_merge_lora: bool,
+    *olive_merge_lora_inputs,
+):
+    unoptimized_dir, optimized_dir = ready(unoptimized_dir, optimized_dir)
+
+    if os.path.isdir(unoptimized_dir):
+        pipeline = load_pipeline(
+            unoptimized_dir,
+            True,
+            torch_dtype=torch.float32,
+            requires_safety_checker=False,
+            local_files_only=True,
+        )
+    else:
+        pipeline = StableDiffusionXLPipeline.from_pretrained(
+            model_id, torch_dtype=torch.float32, requires_safety_checker=False
+        )
+        pipeline.save_pretrained(unoptimized_dir)
+
+    hw_synced = isinstance(sample_size, int)
+
+    submodels = list()
+
+    if text_encoder:
+        submodels.append("text_encoder")
+    if text_encoder_2:
+        submodels.append("text_encoder_2")
+    if unet:
+        submodels.append("unet")
+    if vae_decoder:
+        submodels.append("vae_decoder")
+    if vae_encoder:
+        submodels.append("vae_encoder")
+
+    optimize(
+        unoptimized_dir,
+        optimized_dir,
+        pipeline,
+        vae_id,
+        vae_subfolder,
+        use_fp16,
+        sample_size if hw_synced else sample_size[0],
+        sample_size if hw_synced else sample_size[1],
+        submodels,
+        olive_merge_lora,
+        *olive_merge_lora_inputs,
+    )
+
+    return ["Optimization complete."]
+
+
+def optimize(
+    unoptimized_dir: Path,
+    optimized_dir: Path,
+    pipeline: DiffusionPipeline,
+    vae_id: str,
+    vae_subfolder: str,
+    use_fp16: bool,
+    sample_height: int,
+    sample_width: int,
+    submodels: List[str],
+    olive_merge_lora: bool,
+    *olive_merge_lora_inputs,
+):
+    is_sdxl = "text_encoder_2" in submodels
+    model_info = {}
 
     sample_height_dim = sample_height // 8
     sample_width_dim = sample_width // 8
@@ -86,12 +319,17 @@ def optimize(unoptimized_dir: Path, optimized_dir: Path, pipeline, vae_id: str, 
     os.environ["OLIVE_SAMPLE_WIDTH"] = str(sample_width)
     os.environ["OLIVE_LORA_BASE_PATH"] = str(Path(models_path) / "Lora")
     if olive_merge_lora:
-        os.environ["OLIVE_LORAS"] = '$'.join(olive_merge_lora_inputs)
+        os.environ["OLIVE_LORAS"] = "$".join(olive_merge_lora_inputs)
 
     for submodel_name in submodels:
         print(f"\nOptimizing {submodel_name}")
 
-        with open(Path(sd_configs_path) / ("olive_optimize_sdxl" if is_sdxl else "olive_optimize") / f"config_{submodel_name}.json", "r") as olive_config_raw:
+        with open(
+            Path(sd_configs_path)
+            / ("olive_optimize_sdxl" if is_sdxl else "olive_optimize")
+            / f"config_{submodel_name}.json",
+            "r",
+        ) as olive_config_raw:
             olive_config = json.load(olive_config_raw)
         olive_config["passes"]["optimize"]["config"]["float16"] = use_fp16
 
@@ -113,8 +351,12 @@ def optimize(unoptimized_dir: Path, optimized_dir: Path, pipeline, vae_id: str, 
 
             assert conversion_footprint and optimizer_footprint
 
-            unoptimized_olive_model = ONNXModel(**conversion_footprint["model_config"]["config"])
-            optimized_olive_model = ONNXModel(**optimizer_footprint["model_config"]["config"])
+            unoptimized_olive_model = ONNXModel(
+                **conversion_footprint["model_config"]["config"]
+            )
+            optimized_olive_model = ONNXModel(
+                **optimizer_footprint["model_config"]["config"]
+            )
 
             model_info[submodel_name] = {
                 "unoptimized": {
@@ -138,31 +380,40 @@ def optimize(unoptimized_dir: Path, optimized_dir: Path, pipeline, vae_id: str, 
     kwargs["vae_decoder"] = None
     kwargs["vae_encoder"] = None
     for submodel in submodels:
-        kwargs[submodel] = OnnxRuntimeModel.from_pretrained(model_info[submodel]["unoptimized"]["path"].parent)
+        kwargs[submodel] = OnnxRuntimeModel.from_pretrained(
+            model_info[submodel]["unoptimized"]["path"].parent
+        )
 
-    onnx_pipeline = ORTStableDiffusionXLPipeline(
-        text_encoder_session=kwargs["text_encoder"],
-        text_encoder_2_session=kwargs["text_encoder_2"],
-        unet_session=kwargs["unet"],
-        vae_decoder_session=kwargs["vae_decoder"],
-        vae_encoder_session=kwargs["vae_encoder"],
-        tokenizer=pipeline.tokenizer,
-        tokenizer_2=pipeline.tokenizer_2,
-        scheduler=pipeline.scheduler,
-        feature_extractor=pipeline.feature_extractor,
-        config=dict(pipeline.config),
-    ) if is_sdxl else OnnxStableDiffusionPipeline(**kwargs,
-        tokenizer=pipeline.tokenizer,
-        scheduler=pipeline.scheduler,
-        feature_extractor=pipeline.feature_extractor,
-        requires_safety_checker=False,
+    onnx_pipeline = (
+        ORTStableDiffusionXLPipeline(
+            text_encoder_session=kwargs["text_encoder"],
+            text_encoder_2_session=kwargs["text_encoder_2"],
+            unet_session=kwargs["unet"],
+            vae_decoder_session=kwargs["vae_decoder"],
+            vae_encoder_session=kwargs["vae_encoder"],
+            tokenizer=pipeline.tokenizer,
+            tokenizer_2=pipeline.tokenizer_2,
+            scheduler=pipeline.scheduler,
+            feature_extractor=pipeline.feature_extractor,
+            config=dict(pipeline.config),
+        )
+        if is_sdxl
+        else OnnxStableDiffusionPipeline(
+            **kwargs,
+            tokenizer=pipeline.tokenizer,
+            scheduler=pipeline.scheduler,
+            feature_extractor=pipeline.feature_extractor,
+            requires_safety_checker=False,
+        )
     )
 
     print("Saving unoptimized models...")
     onnx_pipeline.save_pretrained(unoptimized_dir)
 
     print("Copying optimized models...")
-    shutil.copytree(unoptimized_dir, optimized_dir, ignore=shutil.ignore_patterns("weights.pb"))
+    shutil.copytree(
+        unoptimized_dir, optimized_dir, ignore=shutil.ignore_patterns("weights.pb")
+    )
     for submodel_name in submodels:
         try:
             src_path: Path = model_info[submodel_name]["optimized"]["path"]
@@ -177,12 +428,15 @@ def optimize(unoptimized_dir: Path, optimized_dir: Path, pipeline, vae_id: str, 
             pass
 
     with open(optimized_dir / "opt_config.json", "w") as opt_config:
-        json.dump({
-            "sample_height_dim": sample_height_dim,
-            "sample_width_dim": sample_width_dim,
-            "sample_height": sample_height,
-            "sample_width": sample_width,
-        }, opt_config)
+        json.dump(
+            {
+                "sample_height_dim": sample_height_dim,
+                "sample_width_dim": sample_width_dim,
+                "sample_height": sample_height,
+                "sample_width": sample_width,
+            },
+            opt_config,
+        )
 
     shared.refresh_checkpoints()
     print("Optimization complete.")
