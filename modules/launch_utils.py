3f7be4f61b09e18ca90f9095d5c6f1d5e3195e73
Seunghoon Lee <lshqqytiger@naver.com>
2025-04-09 16:30:38 +0900

    update zluda loader

--- modules/launch_utils.py
+++ modules/launch_utils.py
@@ -593,7 +593,6 @@ def prepare_environment():
                     if device is not None and zluda_installer.get_blaslt_enabled():
                         print(f'ROCm hipBLASLt: arch={device.name} available={device.blaslt_supported}')
                         zluda_installer.set_blaslt_enabled(device.blaslt_supported)
-                    zluda_installer.make_copy()
                     zluda_installer.load()
                     torch_command = os.environ.get('TORCH_COMMAND', 'pip install torch==2.6.0 torchvision --index-url https://download.pytorch.org/whl/cu118')
                     print(f'Using ZLUDA in {zluda_installer.path}')
